6b1291ffaf3393a97651471adffdc7ab9713f71b
bigredboots <bigredboots@gmail.com>
2022-04-20 06:37:09 -0600

    filter things

--- src/app/shared/components/vertgrid/vertgrid.component.ts
+++ src/app/shared/components/vertgrid/vertgrid.component.ts
@@ -93,17 +93,17 @@ export class VertgridComponent implements OnInit {
   /* Close fullscreen */
   closeFullscreen(event) {
     this.elem.classList.remove('fullscreened');
-    if (this.document.exitFullscreen) {
-      this.document.exitFullscreen();
-    } else if (this.document.mozCancelFullScreen) {
+    if (this.elem.exitFullscreen) {
+      this.elem.exitFullscreen();
+    } else if (this.elem.mozCancelFullScreen) {
       /* Firefox */
-      this.document.mozCancelFullScreen();
-    } else if (this.document.webkitExitFullscreen) {
+      this.elem.mozCancelFullScreen();
+    } else if (this.elem.webkitExitFullscreen) {
       /* Chrome, Safari and Opera */
-      this.document.webkitExitFullscreen();
-    } else if (this.document.msExitFullscreen) {
+      this.elem.webkitExitFullscreen();
+    } else if (this.elem.msExitFullscreen) {
       /* IE/Edge */
-      this.document.msExitFullscreen();
+      this.elem.msExitFullscreen();
     }
   }
 
